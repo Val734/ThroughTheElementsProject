435c4a53f6be675e5794f2fda0c879393936e21c
Al0nsoB <145582841+Al0nsoB@users.noreply.github.com>
2024-04-20 02:28:23 +0200

    Boss Nivel 1 Y Ataques

---
+++ ThroughTheElementsProject/Assets/Alonso/Nivel_1/Boss/Shaders_Effects/OrbAttack/Scripts/OrbBehaviour.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class OrbBehaviour : MonoBehaviour
+{
+    CharacterController _cController;
+
+    Vector2 randomForce;
+    Vector2 randomAscentForce;
+
+    [SerializeField] float force;
+    [SerializeField] float ascentForce = 10f;
+
+    float lifeTime = 10f;
+
+    [Header("Target Settings")]
+    public Vector3 orbTargetPosition;
+
+    // LAUNCH SETTINGS
+    bool isThrowed;
+    float waitTime = 0.7f;
+
+    private void Awake()
+    {
+        randomForce.x = 15f;
+        randomForce.y = 10f;
+
+        lifeTime = 10f;
+
+        randomAscentForce.x = 10f;
+        randomAscentForce.y = 8f; 
+
+        _cController = GetComponent<CharacterController>();
+
+        force = Random.Range(randomForce.x, randomForce.y);
+    }
+
+    public void ThrowOrb(Vector3 targetPosition)
+    {
+        StartCoroutine(Orblaunch());
+        orbTargetPosition = targetPosition;
+    }
+
+    IEnumerator Orblaunch()
+    {
+        yield return new WaitForSeconds(waitTime);
+        transform.parent = null;
+        isThrowed = true;
+    }
+
+    private void Update()
+    {
+        if (isThrowed)
+        {
+            _cController.Move((orbTargetPosition - transform.position).normalized * (int)force * Time.deltaTime + Vector3.up * ascentForce * Time.deltaTime);
+            
+            lifeTime -= Time.deltaTime;
+            if(lifeTime < 0) 
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.CompareTag("Player")) 
+        {
+            Debug.Log("Ha chocado y tendrá que hacer lo del Hurt Collider");
+
+            Debug.Log("Ahora tiene que instanciar la Explosion de agua para que haga un efecto como que ha explotado el orbe");
+
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ ThroughTheElementsProject/Assets/Alonso/Nivel_1/Boss/Shaders_Effects/WavesAttack/BodyOrb/Scripts/BodyOrbBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyOrbBehaviour : MonoBehaviour
+{
+    [SerializeField] float scaleTime = 5f;
+    [SerializeField] float scaleIncrement = 5f;
+
+    float elapsedTime;
+    Vector3 initialScale;
+
+    private void Awake()
+    {
+        initialScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        float scaleFactor = Mathf.Lerp(0f, scaleIncrement, elapsedTime / scaleTime);
+
+        Vector3 newScale = initialScale + Vector3.one * scaleFactor;
+
+        transform.localScale = newScale;
+
+        if (elapsedTime >= scaleTime)
+        {
+            //Instanciar Partiuclas De Explisión De Agua
+            Destroy(gameObject);
+        }
+    }
+}
