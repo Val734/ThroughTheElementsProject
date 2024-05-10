5f742b39a9eacff7cf3dcbc51865b214f5824fa1
Valeria Rodríguez <145587075+Val734@users.noreply.github.com>
2024-05-10 12:33:33 +0200

    spawnPointComentado porque no va bien
    el botón de continue no va bien

--- ThroughTheElementsProject/Assets/Alonso/Nivel_1/Managers/SpawnPoints/SpawnPointBehaviour.cs
+++ ThroughTheElementsProject/Assets/Alonso/Nivel_1/Managers/SpawnPoints/SpawnPointBehaviour.cs
@@ -1,13 +1,37 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Events;
+//
+//public class SpawnPointBehaviour : MonoBehaviour
+//{
+//    [SerializeField] SpawnManager _spawnManager;
+//    public UnityEvent OnSpawn;
+//
+//
+//    private void Awake()
+//    {
+//        _spawnManager = GetComponentInParent<SpawnManager>();
+//    }
+//
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if(other.CompareTag("Player"))
+//        {
+//            _spawnManager.ChangeSpawnPoint(transform);
+//            gameObject.SetActive(false);
+//            OnSpawn.Invoke();
+//        }
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class SpawnPointBehaviour : MonoBehaviour
 {
     [SerializeField] SpawnManager _spawnManager;
-    public UnityEvent OnSpawn;
-
 
     private void Awake()
     {
@@ -16,11 +40,10 @@ public class SpawnPointBehaviour : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             _spawnManager.ChangeSpawnPoint(transform);
             gameObject.SetActive(false);
-            OnSpawn.Invoke();
         }
     }
 }
